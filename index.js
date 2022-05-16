edaa1015f4a2c29a2c50dee0a7ba4b3af7bceeaa
Danny <josedan.jd94@gmail.com>
2022-05-16 12:51:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+let map, infoWindow;
+function initMap() {
+  let options = {
+    zoom: 13,
+    center: { lat: 23.634501, lng: -102.552784 },
+  };
+
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  //infoWindow
+  infoWindow = new google.maps.InfoWindow({
+    content: "<h3>Hey estoy aquí</h3>",
+  });
+
+  //Agregar marcardor
+  const addMarker = (coords) => {
+    const marker = new google.maps.Marker({
+      position: coords,
+      map,
+      animation: google.maps.Animation.DROP,
+    });
+
+    marker.addListener("click", () => {
+      infoWindow.setPosition(coords);
+      infoWindow.setContent("<h3> Hey estoy aqui</h3>");
+      infoWindow.open(map, marker);
+    });
+
+    map.setCenter(coords);
+
+  };
+
+  const locationButton = crearBoton();
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+  locationButton.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({coords}) => {
+          const pos = {
+            lat: coords.latitude,
+            lng: coords.longitude,
+          };
+          addMarker(pos); //Agregar marcador a mi ubicacion
+        }
+      );
+    }
+  });
+}
+
+const crearBoton = () => {
+  const btnUbicacion = document.createElement("button");
+  btnUbicacion.textContent = "Ir a mi ubicación";
+  btnUbicacion.classList.add("btn");
+  btnUbicacion.classList.add("btn-primary");
+
+  return btnUbicacion;
+}
